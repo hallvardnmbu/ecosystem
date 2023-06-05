31ec34511114df5fc56a773596d8b7d9d5467ab2
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-06-05 13:33:46 +0200

    Created different functions for the superclass: aging, lose_weight etc.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,6 +1,23 @@
+import random
+random.seed(1)
+
 class Animal:
     @classmethod
     def set_parameters(cls, parameters=None):
+        """
+        Set the parameters for an animal.
+
+        Parameters
+        ----------
+        - parameters : dict
+            {"parameter": value}
+
+        Raises
+        ------
+        - ValueError
+            If invalid parameter values are passed.
+        """
+
         if parameters is None:
             return
         class_parameters = cls.default_parameters()
@@ -12,8 +29,57 @@ class Animal:
 
     def __init__(self, position, weight, age):
         self.position = position
-        self.weight = weight
         self.age = age
+        if not weight:
+            self.weight = random.lognormal(self.parameters["w_birth"], self.parameters["sigma_birth"])
+        else:
+            self.weight = weight
+
+    def aging(self):
+        """
+        Increments the age of the animal by one year.
+        """
+
+        self.age += 1
+
+    def gain_weight(self, food):
+        """
+        Increments the weight of the animal by the factor beta and the amount of food eaten.
+        """
+
+        self.weight += self.parameters["beta"] * food
+
+    def lose_weight(self):
+        """
+        Decrements the weight of the animal by the factor eta.
+        """
+
+        self.weight -= self.parameters["eta"] * self.weight
+
+    def lose_weight_birth(self, baby_weight):
+        """
+        Decrements the weight of the animal by the factor xi and the weight of the baby.
+        """
+
+        self.weight -= self.parameters["xi"] * baby_weight
+
+    def baby_weight(self):
+        """
+        Calculates the weight of the baby.
+        """
+
+        if random.random() < min(1, self.parameters["gamma"] * self.fitness * 10): # BYTT "10" MED: "self.count_animals_cell()" COUNT ANIMAL CELL!):
+            return random.lognormal(self.parameters["w_birth"], self.parameters["sigma_birth"])
+
+    def give_birth(self, baby_weight):
+        """
+        Creates a new animal with the given weight at the same position as the parent.
+        """
+
+        if baby_weight > self.weight:
+            return
+        self.lose_weight_birth(baby_weight)
+        self.__class__(position=self.position, weight=baby_weight)
 
     @property
     def fitness(self):
@@ -23,6 +89,10 @@ class Animal:
 class Herbivore(Animal):
     @classmethod
     def default_parameters(cls):
+        """
+        Default parameters for Herbivores.
+        """
+
         return {"w_birth": 8.0,
                 "sigma_birth": 1.5,
                 "beta": 0.9,
@@ -45,6 +115,10 @@ class Herbivore(Animal):
 class Carnivore(Animal):
     @classmethod
     def default_parameters(cls):
+        """
+        Default parameters for Carnivores.
+        """
+
         return {"w_birth": 6.0,
                 "sigma_birth": 1.0,
                 "beta": 0.75,
@@ -60,6 +134,7 @@ class Carnivore(Animal):
                 "omega": 0.8,
                 "F": 50.0,
                 "DeltaPhiMax": 10.0}
+
     def __init__(self, position, weight=None, age=0):
         super().__init__(position, weight, age)
         self.parameters = self.default_parameters()
