c64c1cf6d0197a1828505053fbddfa1b0d3193e4
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-06-05 11:52:50 +0200

    Created the __init__-function for each of the super- and subclasses.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,12 +1,13 @@
 class Animal:
-    def __init__(self, age=0, weight):
-        self.age = age
+    def __init__(self, position, weight, age):
+        self.position = position
         self.weight = weight
+        self.age = age
 
 class Herbivore(Animal):
-    def __init__(self):
-        pass
+    def __init__(self, position, weight=None, age=0):
+        super().__init__(position, weight, age)
 
 class Carnivore(Animal):
-    def __init__(self):
-        pass
+    def __init__(self, position, weight=None, age=0):
+        super().__init__(position, weight, age)
