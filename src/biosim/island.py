7f168cd19505d9b2e6e985bf006a2bbfadd8fbc0
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-06-05 14:57:23 +0200

    Notes from presentation (2); Cell-class.

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -1,6 +1,10 @@
 # class Cell:
-    # {herbivores: [], carnivores: []}
+    # {Herbivores: [], Carnivores: []}
     # __init__ ^
     # Get older
     # Lose weight
     # etc.
+
+    # Die
+        # Loop over elements in lists in dict,
+        # remove dead animals from list(s)
