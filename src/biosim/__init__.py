8cbb51c9a7218e89ba0035356192056e42dc9543
Thyra Martinsen <thyra.martinsen@nmbu.no>
2023-06-09 09:12:53 +0200

    typo

--- src/biosim/__init__.py
+++ src/biosim/__init__.py
@@ -1,3 +1,3 @@
-__author__ = 'Hallvard høyland Lavik' ,'Thyra Martinsen'
+__author__ = 'Hallvard Høyland Lavik' ,'Thyra Martinsen'
 __email__ = 'hallvard.hoyland.lavik@nmbu.no', 'thyra.martinsen@nmbu.no'
 __version__ = '0.1'
