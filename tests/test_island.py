26123600c3521ee74dcddf10da64493846badf41
Thyra Martinsen <thyra.martinsen@nmbu.no>
2023-06-09 13:23:10 +0200

    update so we can work together

--- tests/test_island.py
+++ tests/test_island.py
@@ -8,7 +8,33 @@ import pytest
 # Should write a docstring to further explain the test
 # organise test types in classes?
 
-SEED = 112233
+# Overall parameters for probabilistic tests
+SEED = 112233   # random seed for tests
+ALPHA = 0.01  # significance level for statistical tests
+
+@pytest.fixture
+def reset_params():
+    """
+    Fixture resetting class parameters on Bacteria.
+
+    The fixture resets Bacteria class-level parameters before leaving a test.
+    """
+
+    # no set-up
+    yield
+    Island.set_fodder_parameters(default_fodder_parameters())
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @pytest.fixture
@@ -24,9 +50,9 @@ def make_test_island():
         to test functions on."""
 
     test_geography = """\
-                        WWWWW
-                        WWLWW
-                        WWWWW"""
+                        WWWWWW
+                        WWLHWW
+                        WWWWWW"""
 
     test_pop = [{"loc": (2, 3),"pop": [{"species": "Carnivore" }]}]
 
@@ -41,3 +67,13 @@ def test_add_population():
     test_island.add_population([{"loc": (2, 3),"pop": [{"species": "Herbivore" }]}])
     assert test_island.n_animals == 2, "The population was not added correctly."
 
+def test_migration():
+
+    """Tests that the migration is performed correctly. Give the carnivor
+    really good fitness so that it will migrate with high probability.
+    """
+    test_island=make_test_island()
+
+    test_island.migration()
+    assert test_island.n_animals != 1, "The migration was not performed correctly."
+    assert
