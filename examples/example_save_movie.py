a387b4ac13794c39906078bc9551bf7c9b09798f
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-06-16 18:16:49 +0200

    Fixed GUI (transpose map when simulating) and creates example scripts.

--- examples/example_save_movie.py
+++ examples/example_save_movie.py
@@ -1,10 +1,45 @@
 """
-This is a small demo script running a randvis simulation and generating a movie.
+This is a small demo script running a BioSim simulation and generating a movie.
 """
 
-from randvis.simulation import DVSim
+from src.biosim.simulation import BioSim
 
-sim = DVSim((10, 15), 0.1, 12345, '../data')
-sim.simulate(100, 1, 5)
-sim.make_movie('mp4')
-sim.make_movie('gif')
+nmbu = """\
+          WWWWWWWWWWWWWWWWWWWWWWWWWWW
+          WWWWWWWWWWWWWWWWWWWLLLWLLLW
+          WWWWWWWWWWWWWWWWWWWLDLWLDLW
+          WWWWWWWWWLLLLLLLLLLLDLWLDLW
+          WWWWWWWWWLDDDDDDDDDLDLWLDLW
+          WWWWWWWWWLDLLLLLLLLLDLLLDLW
+          WWWWWWWWWLDLWWWWWWWLDDDDDLW
+          WWWWWWWWWLDLWLLLLLLLLLLLLLW
+          WWWWWWWWWLDLWLDDDDLWWLDLWWW
+          WWWLLLLLLLDLLLDLLDLLWLDLWWW
+          WWWLDDDDDDDDDLDLDDDLWLDLWWW
+          WWWLDLLLLLDLLLDLLLDLWLDLWWW
+          WWWLDLWWWLDLWLDDDDDLWLDLWWW
+          WWWLDLWLLLLLLLLLLLLLWLDLWWW
+          WWWLDLWLDDLDDLWLDLWWWLDLWWW
+          WWWLDLWLDDDDDLLLDLLLLLDLWWW
+          WWWLDLWLDLDLDLDDDDDDDDDLWWW
+          WWWLDLWLDLLLDLLLDLLLLLLLWWW
+          WWWLDLWLDLWLDLLLDLWWWWWWWWW
+          WLLLLLLLLLWLLLWLDLWWWWWWWWW
+          WLDDLLDLWWWWWWWLDLWWWWWWWWW
+          WLDDDLDLLLLLLLLLDLWWWWWWWWW
+          WLDLDLDLDDDDDDDDDLWWWWWWWWW
+          WLDLDDDLLLLLLLLLLLWWWWWWWWW
+          WLDLLDDLWWWWWWWWWWWWWWWWWWW
+          WLLLLLLLWWWWWWWWWWWWWWWWWWW
+          WWWWWWWWWWWWWWWWWWWWWWWWWWW"""
+
+population = [{"loc": (14, 14),
+               "pop": [{"species": "Herbivore", "age": 5, "weight": 20} for _ in range(150)]},
+              {"loc": (14, 14),
+               "pop": [{"species": "Carnivore", "age": 5, "weight": 20} for _ in range(40)]}]
+
+sim = BioSim(island_map=nmbu, ini_pop=population, seed=123456, vis_years=1,
+             img_dir='demo_movie', img_base='demo')
+
+sim.simulate(200)
+sim.make_movie()
--- examples/example_with_gui.py
+++ examples/example_with_gui.py
@@ -1,3 +1,7 @@
+"""
+This is a small demo script running BioSim with the Graphical User Interface (GUI).
+"""
+
 from biosim.gui import BioSimGUI
 
 BioSimGUI(map_size=20).mainloop()
